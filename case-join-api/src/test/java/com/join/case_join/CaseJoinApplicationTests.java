255666a6de53b96afcdc3f6011dd34546c5cfd17
Wellington <wellingtongmb@gmail.com>
2025-02-12 18:14:49 -0300

    init project with spring boot 3.4.2, java 21, React with vite, Postgre database

---
+++ case-join-api/src/test/java/com/join/case_join/CaseJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.join.case_join;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseJoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
